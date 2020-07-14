62493ab6604ec529f1ee518927cfd372924c2907
lehel <mezei.lehel@webtown.hu>
2020-07-14 12:34:45 +0200

    Repository interfaces added.

---
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author lehel
+ */
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
