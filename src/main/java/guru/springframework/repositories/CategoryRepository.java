23afc451fd279bc3825074152e2172b20504d399
lehel <mezei.lehel@webtown.hu>
2020-07-15 13:47:26 +0200

    database initialization data added. Repositories created for UOM and Category.

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -2,9 +2,14 @@ package guru.springframework.repositories;
 
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * @author lehel
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
